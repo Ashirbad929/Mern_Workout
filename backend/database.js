01ca687129cdb6275903b8e9a46c9ecae933e71c
ASHIRBAD BEHERA <96345801+Ashirbad929@users.noreply.github.com>
2023-05-29 13:48:02 +0530

    Update database.js

--- backend/database.js
+++ backend/database.js
@@ -1,15 +1,16 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-const mongoose_connect = () => {
-    
-    mongoose.connect(`${process.env.MONGO_URI}`, { useNewUrlParser: true })
-    .then(() => {
-      console.log('mongodb Connection successful')
-    })
-    .catch(error => {
-      // Handle error
-      console.log('error while connecting database')
+const connectToMongoDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 0, // Set timeout to infinity
     });
-  
-}
-export default mongoose_connect;
+    console.log('MongoDB connection successful');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+};
+
+export default connectToMongoDB;
